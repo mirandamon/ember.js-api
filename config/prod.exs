cea2cc5ce02f330a4cc6fc2b19d4d12d53d51dba
mirandamon <nmiranda4@fordham.edu>
2016-05-11 01:42:49 -0400

    Ready for Heroku

--- config/prod.exs
+++ config/prod.exs
@@ -15,8 +15,7 @@ config :peepchat, Peepchat.Endpoint,
   http: [port: {:system, "PORT"}],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   url: [host: "still-woodland-92248.herokuapp.com", port: 443],
-  secret_key_base: System.get-env("SECRET_KEY_BASE"),
-  cache_static_manifest: "priv/static/manifest.json"
+  secret_key_base: System.get_env("SECRET_KEY_BASE")
 
 config :peepchat, Peepchat.Repo,
   adapter: Ecto.Adapters.Postgres,
